7ddf2ddfd83660b0659fad059bf0c147ba0eb2c8
muta332 <playplay2627@gmail.com>
2025-11-28 12:07:13 +0600

    Summery
    -Rifiling System
    -Granade System(incomplete)
    -Anim System
    Right now Only available For players. Working on it to make all the sytems modular for AI and Behaviour tree

---
+++ Source/AdvanceAI/AdvanceAI.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AdvanceAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AdvanceAI, "AdvanceAI" );
+ 
